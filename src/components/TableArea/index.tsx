d3bab298d15841c44767f878378bae51355c27e6
M4rcosFelipe <marcosfelipe399@hotmail.com>
2022-10-26 12:36:28 -0300

    Feat: Adicionando review

--- src/components/TableArea/index.tsx
+++ src/components/TableArea/index.tsx
@@ -1,34 +1,48 @@
-import { Item } from "../../types/Item"
-import { TableItem } from "../TableItem"
-import { Table, TableHeadColumn } from "./styles"
+import { Item } from "../../types/Item";
+import { TableItem } from "../TableItem";
+import { Table, TableHeadColumn } from "./styles";
 
 type Props = {
-    list: Item[]
-}
+  list: Item[];
+};
 
-export const TableArea = ({list}: Props) =>{
+export const TableArea = ({ list }: Props) => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <TableHeadColumn>Data</TableHeadColumn>
+          <TableHeadColumn>Categoria</TableHeadColumn>
+          <TableHeadColumn>Título</TableHeadColumn>
+          <TableHeadColumn>Valor</TableHeadColumn>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Acho que o <TableItem/> não precisa ser um componente separado, já que ele necessariamente vai estar dentro de uma tabela e não seria reutilizado em algum outro lugar fora dessa tabela */}
+        {list.map((item, index) => (
+          <TableItem key={index} item={item} />
 
+          /*
 
-    return(
-        <Table>
-            <thead>
-                <tr>
-                    <TableHeadColumn>Data</TableHeadColumn>
-                    <TableHeadColumn>Categoria</TableHeadColumn>
-                    <TableHeadColumn>Título</TableHeadColumn>
-                    <TableHeadColumn>Valor</TableHeadColumn>
-                </tr>
+                            <TableLine>
+                                <TableColumn>{formatDate(item.date)}</TableColumn>
+                                <TableColumn>
+                                    <ColorCategory color={categories[item.category].color} >
+                                        {categories[item.category].title}
+                                    </ColorCategory>
+                                </TableColumn>
+                                <TableColumn> {item.title} </TableColumn>
+                                <TableColumn>
+                                    <ColorPrice  color={categories[item.category].expense ? 'red' : 'green'} >
+                                    R$ {item.price} 
+                                    </ColorPrice>
+                                </TableColumn>
 
+                            </TableLine>
 
-            </thead>
-            <tbody>
-                {list.map((item, index)=>(
-                    <TableItem key={index} item={item}/>
-                   ))}
-            </tbody>
-
-
-        </Table>
-
-    )
-}
+                    */
+        ))}
+      </tbody>
+    </Table>
+  );
+};
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,12 +1,34 @@
-import { Category } from "../types/Category"
+import { Category } from "../types/Category";
+
+/*
+- Particularmente eu colocaria a tag das categorias ou em letras minúsculas, sem acentos e sem caracteres especiais, porque é menos suscetível à erros ou então preferencialmente em inglês.
 
 export const categories: Category = {
-    'Alimentação': {title: 'Alimentação', color: 'red', expense: true},
-    'Saúde': {title: 'Saúde', color: 'blue', expense: true},
-    'Salário': {title: 'Salário', color: 'green', expense: false},
-    'Desp. Residencial': {title: 'Desp. Residencial', color: 'purple', expense: true},
-    'Aleatórios': {title: 'Aleatórios', color: 'brown', expense: true},
-  
-    
+  food: { title: "Alimentação", color: "red", expense: true },
+  health: { title: "Saúde", color: "blue", expense: true },
+  salary: { title: "Salário", color: "green", expense: false },
+  Desp: { title: "Desp. Resid", color: "purple", expense: true },
+  random: { title: "Aleatórios", color: "brown", expense: true },
+};
 
-}
+ Ou, ao invés de buscá-las pelo nome, transformar o objeto "categories" em um array, já que é uma lista de categorias,colocar um id em cada item,e usar esse id para a busca:
+
+
+ export const categories: Category = [
+   {id:1, title: "Alimentação", color: "red", expense: true },
+   {id:2, title: "Saúde", color: "blue", expense: true },
+   {id:3, title: "Aleatórios", color: "brown", expense: true },
+];
+
+*/
+export const categories: Category = {
+  Alimentação: { title: "Alimentação", color: "red", expense: true },
+  Saúde: { title: "Saúde", color: "blue", expense: true },
+  Salário: { title: "Salário", color: "green", expense: false },
+  "Desp. Residencial": {
+    title: "Desp. Residencial",
+    color: "purple",
+    expense: true,
+  },
+  Aleatórios: { title: "Aleatórios", color: "brown", expense: true },
+};
